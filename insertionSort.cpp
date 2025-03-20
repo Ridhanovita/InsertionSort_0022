2dea83e875fff1090daaa9b35a8cef4827760b77
Ridhanovita <novitaridha796@gmail.com>
2025-03-20 16:06:21 +0700

    Fungsi untuk menampilkan Array yang telah terurut

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -50,3 +50,19 @@ void insertion()
         arr[j + 1] = temp; //step 5
     }
 }
+
+void display()
+{
+    cout << endl; //Output baris kosong
+    cout << "================================" << endl;
+    cout << "Total Pass = " << n - 1 << endl; //count element movement
+    cout << "================================" << endl; //output ke layar
+    cout << "Element Array yang telah tersusun" << endl; //Output ke layar
+    cout << "================================" << endl; //Output ke layar
+
+    for (int j = 0; j < n; j++)
+    { //Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl; //output ke layar
+    }
+    cout << endl; //Output baris kosong
+}
